3b3e6f27ae1f37d794d2ad5df162556823c854f8
Houtaro <cungvietanh04@gmail.com>
2025-08-10 14:21:09 +0700

    fixed error

--- src/Components/ChatRoom/index.jsx
+++ src/Components/ChatRoom/index.jsx
@@ -1,22 +1,13 @@
 import React from "react";
 import {Row, Col} from 'antd';
-<<<<<<< HEAD
 import ChatWindow from "./ChatWindow.jsx";
 import SideBar from "./SideBar.jsx";
-=======
-import ChatWindow from "./ChatWindow";
-import Sidebar from "./Sidebar";
->>>>>>> a8db88875c664bdd6cc575df4f6c3ecd014aa843
 
 export default function ChatRoom() {
   return (
         <Row>
           <Col span={6}>
-<<<<<<< HEAD
             <SideBar />
-=======
-            <Sidebar />
->>>>>>> a8db88875c664bdd6cc575df4f6c3ecd014aa843
           </Col>
           <Col span={18}>
             <ChatWindow />
